9c8daf5818534f3b899dd8691098d4c8c293a041
Rajabalumathi <rrajabalumathi@gmail.com>
2020-12-09 16:22:51 +0530

    GasBooking

---
+++ GasBooking/src/test/java/com/gasbooking/GasBooking/GasBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasbooking.GasBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
